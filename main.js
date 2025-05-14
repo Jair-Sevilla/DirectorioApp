081f6d30992a97da07180e3465a61b290e55c11e
Jair-Sevilla <alejandrosevilla66044@gmail.com>
2025-05-14 11:32:03 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log ("Hola");
+//En java script tenemos las funciones tipo flecha
+//Utilizaremos fetch que es un metodo para consumir una API
+
+//Paso 1: Funcion flecha
+const consumirApi = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    //Convertimos la respuesta a tipo json
+    .then((response) => response.json())
+    //Los datos los vamos a mandar a la consola
+    .then((data) => console.log(data))
+    //Descubrir que hacer en caso de que no me responda Error 404 not found
+    .catch((error) => console.log(error));
+      };
+
+consumirApi();   
+   
+//TAREA en ingles hacer todo
+//Consumir otra API y mostrar la data en consola como la de arriba

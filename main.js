bb52f1884f57492bfa185a28d9ab1654ae7a9063
Jair-Sevilla <alejandrosevilla66044@gmail.com>
2025-05-17 16:47:15 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,21 @@ const consumirApi = () => {
     .catch((error) => console.log(error));
       };
 
-consumirApi();   
+ //API 2:
+ const segundaAPI = () => { 
+  //Enlace de la API -- API Link
+  fetch("https://jsonplaceholder.typicode.com/posts")  
+  //Convertimos la respuesta a tipo json -- We convert the response to json type
+  .then((response) => response.json())
+  //Los datos los vamos a mandar a la consola -- We are going to send the data to the console
+  .then((data) => console.log(data))
+  //Descubrir que hacer en caso de que no me responda -- Find out what to do if he doesn't answer me -- Error 404 not found
+  .catch((error) => console.log(error));
+};
+
+consumirApi();  
+
+segundaAPI(); 
    
 //TAREA en ingles hacer todo
 //Consumir otra API y mostrar la data en consola como la de arriba

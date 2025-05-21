a577f05dad7a1aa4bf961a7a7104061b73ce1cee
Jair-Sevilla <alejandrosevilla66044@gmail.com>
2025-05-21 11:34:20 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -15,25 +15,34 @@ const consumirApi = () => {
       const nombreUsuario = document.getElementById("nombreUsuario");
       const email = document.getElementById("email");
       const phone = document.getElementById("phone");
+      const street = document.getElementById("street");
+      const contenedor = document.getElementById("contenedor");
 
       nombre.innerText = data[0].name; //name no existe proviene desde la API
       nombreUsuario.innerText = data[0].username;
       email.innerText = data[0].email;
       phone.innerText = data[0].phone;
       website.innerText = data[0].website; 
+      //street.innerText = data [0].address.street;
+      //innerHTML inserta html directo/ innerText es texto normal
+      street.innerHTML= `<h1>${data[0].address.street}</h1>`
 
-      data.forEach(usuario => {
-      //Obtenemos los datos usando el metodo forEach que pasara por cada uno de los usuarios 
-      //$ es utilizado para incluir valores o expresiones dentro de una cadena de texto
-      const direccion = `${usuario.address.street}, ${usuario.address.suite}, ${usuario.address.city}, ${usuario.address.zipcode}`;
-      
-      const item = document.createElement('li');
-      item.textContent = direccion;
-      //appendchild es un metodo que sirve para meter un objeto dentro de otro
-      lista.appendChild(item);
-    });
-
+      //Aqui vamos a facilitarnos la vida con forEach
       //forEach es un metodo de arreglos que ejecuta una funcion por cada elemento que esta en el arreglo
+      data.forEach((usuario, index)=>
+        //+= es como añadir 
+        contenedor.innerHTML+=`
+        <p>${usuario.name}</p>
+        <p>${usuario.username}</p>
+        <p>${usuario.email}</p>
+        <p>${usuario.phone}</p>
+        <p>${usuario.website}</p>
+        <p>${usuario.address.street}</p>
+        <hr>
+    
+        `);
+         //<p> es parrafo
+        //<hr> es un salto de renglon
     })
 
     //Descubrir que hacer en caso de que no me responda Error 404 not found
